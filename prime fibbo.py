47da7ad0e5ed2c4679d6d46ff550bdf480191672
Mansi Kaushik <mansi.17bec1145@abes.ac.in>
2019-06-17 17:31:54 +0530

    this is my first commit

---
+++ prime fibbo.py
@@ -0,0 +1,16 @@
+n=int(input("print number of primes in fibbo"))
+a,b=0,1
+for i in range(0,n-2):
+    c=a+b
+    if c%2 is 0:
+        print("not prime")
+    else:
+        for j in range(2,c):
+            if c%j is 0:
+                    print("not prime")
+            else:
+                    print(c)
+            
+		
+                    
+        
